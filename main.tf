b897263f8a8f7bf7815203767e14ba0099f9ebe3
nikhil0499 <109714712+nikhil0499@users.noreply.github.com>
2024-01-15 16:56:41 +0530

    Testing 2

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.87.0"
-    }
-  }
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-provider "azurerm" {
-  # Configuration options
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+resource "random_string" "azurerm_api_management_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_api_management" "api" {
+  name                = "apiservice${random_string.azurerm_api_management_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_email     = var.publisher_email
+  publisher_name      = var.publisher_name
+  sku_name            = "${var.sku}_${var.sku_count}"
 }
